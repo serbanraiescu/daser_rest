5f60490ec5f968f5cdd0ffa2167da5f24dfc85c0
serbanraiescu <serbanraiescu@gmail.com>
2026-05-10 15:25:00 +0300

    fix: allow Filament access in production environment

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,9 +6,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
+    }
     use HasFactory, Notifiable;
 
     /**
